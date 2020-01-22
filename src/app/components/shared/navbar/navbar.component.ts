2640d67e02089335af24c9ae2881662849b47b4d
MatiasM <matias_monasterio@outlook.com>
2020-01-22 18:19:25 -0300

    Diseño y funciones del navbar

--- src/app/components/shared/navbar/navbar.component.ts
+++ src/app/components/shared/navbar/navbar.component.ts
@@ -7,9 +7,43 @@ import { Component, OnInit } from '@angular/core';
 })
 export class NavbarComponent implements OnInit {
 
+  show: boolean = false;
+  dropdownMenu: boolean = false;
+  scrollResponsive: boolean;
+
+  logo: HTMLElement;
+
   constructor() { }
 
   ngOnInit() {
+    this.logo = document.querySelector('#logo');
+    this.scrollNav();
+  }
+
+  // MUESTRA/ OCULTA EL SIDE NAV
+  showSideNav(){
+    this.show = !this.show;
+    this.dropdownMenu = false;
+  }
+
+  // MUESTRA/ OCULTA EL DROPDOWN MENU
+  showDropdownMenu(){
+    this.dropdownMenu = !this.dropdownMenu;
+  }
+
+  // CAMBIAR TAMAÑO DEL NAV AL REALIZAR SCROLL
+  scrollNav(){
+    let self = this;
+    window.onscroll = function(){
+      if(window.scrollY >= 10) {
+        self.scrollResponsive = true;
+        self.logo.style.width = '110px';
+      }
+      else {
+        self.scrollResponsive = false;
+        self.logo.style.width = '150px';
+      }
+    } 
   }
 
 }
