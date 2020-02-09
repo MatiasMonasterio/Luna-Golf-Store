0dcf8b46e34eaa206682d0ba44a2836fab520000
MatiasM <matias_monasterio@outlook.com>
2020-02-09 15:55:00 -0300

    Se agrega componente en el modulos products

--- src/app/components/products/products.module.ts
+++ src/app/components/products/products.module.ts
@@ -1,23 +1,26 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
+// RUTAS
 import { AppRoutingChildModule } from './products-routing.module';
 
+// COMPONENTES
 import { ProductsComponent } from './products.component';
 import { PalosComponent } from './palos/palos.component';
 import { PelotasComponent } from './pelotas/pelotas.component';
 import { BolsosComponent } from './bolsos/bolsos.component';
 import { CarrosComponent } from './carros/carros.component';
 import { AccesoriosComponent } from './accesorios/accesorios.component';
-import { ProductsViewComponent } from './shared/products-view/products-view.component';
 
+// COMPONENTES COMPARTIDOS
 import { ProductComponent } from './shared/product/product.component';
+import { ProductsViewComponent } from './shared/products-view/products-view.component';
+import { CardComponent } from './shared/card/card.component';
 
 // PIPES
 import { NombrePipe } from '../../pipes/nombre.pipe';
 import { DetallesPipe } from '../../pipes/detalles.pipe';
 import { PrecioPipe } from '../../pipes/precio.pipe';
-
 
 
 @NgModule({
@@ -32,7 +35,8 @@ import { PrecioPipe } from '../../pipes/precio.pipe';
     ProductComponent,
     NombrePipe,
     DetallesPipe,
-    PrecioPipe
+    PrecioPipe,
+    CardComponent
   ],
   imports: [
     CommonModule,
