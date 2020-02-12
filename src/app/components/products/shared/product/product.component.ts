eb7e3f71afff62bcd1dcaea2c7150edfb2d5a4cc
MatiasM <matias_monasterio@outlook.com>
2020-02-12 15:52:25 -0300

    Mejoras en diseño y funcionamiento del product component

--- src/app/components/products/shared/product/product.component.ts
+++ src/app/components/products/shared/product/product.component.ts
@@ -1,14 +1,16 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { ProductsService } from '../../products.service';
 import { Producto } from '../../../../interface/producto';
+import { Router, NavigationStart } from '@angular/router';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-product',
   templateUrl: './product.component.html',
   styleUrls: ['./product.component.scss']
 })
-export class ProductComponent implements OnInit {
+export class ProductComponent implements OnInit, OnDestroy {
 
   producto: Producto = {
     nombre: '',
@@ -20,20 +22,28 @@ export class ProductComponent implements OnInit {
   };
 
   relacionados: Array<Producto> = [];
+  routerSubscription: Subscription;
 
-  constructor( private ar: ActivatedRoute, private _ps: ProductsService ){
+  constructor( private ar: ActivatedRoute, private _ps: ProductsService, private router: Router){
     this.getParameter();
+    this.routerNavigat();
   }
 
   ngOnInit() {
   }
 
+  ngOnDestroy(){
+    if( this.routerSubscription !== undefined ) this.routerSubscription.unsubscribe();
+  }
+
+  // OBTIENE EL ID DEL PRODUTO DESDE LOS PARAMETROS DE LA URL
   getParameter(){
     this.ar.params.subscribe( valor=>{
       this.getProduct( valor['id'] );
     })
   }
 
+  // TRAE EL PRODUCTO CON EL ID OBTENDIO
   getProduct( id: string ){
     this._ps.getProduct(id)
       .subscribe(resp=>{
@@ -42,12 +52,23 @@ export class ProductComponent implements OnInit {
       })
   }
 
+  // ARRAY CON 4 PRODUCTOS CUALESQUIERA
   createProductosRelacionados(){
     for( let i = 0 ; i < 4 ; i++ ){
       this.relacionados.push(this.producto);
     }
+  }
 
-    console.log(this.relacionados);
+  // VERIFICA SI SE NAVEGA ENTRE LOS RELACIONADOS O SE VEULVE PARA ATRAS PARA 
+  // CONSERVAR O ELIMINAR LA INFORMACION DEL PRODUCT-VIEW
+  routerNavigat(){
+    this.routerSubscription = this.router.events.subscribe((e)=>{
+      if(e instanceof NavigationStart){
+        if( !e.url.includes('/producto/') && e.navigationTrigger !== 'popstate'){
+          this._ps.getProductos( 'categoria' , true );
+        }
+      }
+    })
   }
 
 }
