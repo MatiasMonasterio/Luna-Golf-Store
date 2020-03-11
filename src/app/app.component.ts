ce5206d8355ceaf86e40f48f8b70ae2c4a787ce3
MatiasM <matias_monasterio@outlook.com>
2020-03-11 20:02:59 -0300

    Se agrega boton para hacer scroll hacia arriba

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,6 +5,19 @@ import { Component } from '@angular/core';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent{
   title = 'LunaGolfStore';
+
+  upBtn: boolean = false;
+
+  constructor(){
+    window.addEventListener('scroll',()=>{
+      if(window.scrollY >= 800 ) this.upBtn = true ;
+      else this.upBtn = false;
+    })
+  }
+
+  up(){
+    window.scroll( 0, 0 );
+  }
 }
